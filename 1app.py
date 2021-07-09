45424868cc383eb34c6b3f198de26029f0e932dd
ShashankHebbarS <72264018+ShashankHebbarS@users.noreply.github.com>
2021-07-09 09:52:18 +0530

    Titanic Prediction App
    Add new project Titanic prediction.
    some minor tweaks

--- 1app.py
+++ 1app.py
@@ -7,26 +7,54 @@ app = Flask(__name__)
  
 @app.route('/')   
 def home():  
-    return render_template("Home.html"); 
+    return render_template("Home.html")
+
+
 
 @app.route('/IrisClass')
 def IrisClass():
     return render_template('Iris.html')
 
-def predictor(inp):
+def irispred(inp):
     arr = np.array(inp).reshape(1, 4)
-    mod = pickle.load(open('iris.pkl', 'rb'))
-    result = mod.predict(arr)
+    iris_model = pickle.load(open('iris.pkl', 'rb'))
+    result = iris_model.predict(arr)
     return result[0]
 
-@app.route('/results', methods=['POST'])
-def result():
+@app.route('/IrisClass', methods=['POST'])
+def IrisResult():
+    if request.method == 'POST':
+        inp = request.form.values()
+
+        inp = list(map(float, inp))
+        result = irispred(inp)
+
+        return render_template('Iris.html', predictions=result)
+
+
+
+@app.route('/TitanicPred')
+def TitanicPred():
+    return render_template('Titanic.html')
+
+def titanicpred(inp):
+    arr = np.array(inp).reshape(1, 6)
+    titanic_model = pickle.load(open('titanic.pkl', 'rb'))
+    result = titanic_model.predict(arr)
+    return result[0]
+
+@app.route('/TitanicPred', methods=['POST'])
+def titanicresult():
     if request.method == 'POST':
         inp = request.form.values()
         inp = list(map(float, inp))
-        result = predictor(inp)
+        result = titanicpred(inp)
 
-        return render_template('Iris.html', predictions=result)
+        if result == 0:
+            return render_template('Titanic.html', predictions='The Passenger Died')
+
+        else:
+            return render_template('Titanic.html', predictions='The Passenger Survived')
 
 
 if __name__ =='__main__':  
